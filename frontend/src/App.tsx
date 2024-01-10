db9d14ee1b428d7c20953db446a0f002d0f202af
jumpinggoofballs <jumpinggoofballs@users.noreply.github.com>
2024-01-10 12:49:28 +0000

    Initial setup for colour toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,26 @@
 import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 export default function App() {
-
-  let toggle = false
+  let toggle = false;
 
   return (
-    <section className={`h-full bg-white ${ toggle ? 'invert' : ''}`}>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+    <section className={`h-full bg-white ${toggle ? "invert" : ""}`}>
+      <Router>
+        <EntryProvider>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/" element={<AllEntries />}></Route>
+            <Route path="create" element={<NewEntry />}></Route>
+            <Route path="edit/:id" element={<EditEntry />}></Route>
+          </Routes>
+        </EntryProvider>
+      </Router>
     </section>
-    
   );
 }
